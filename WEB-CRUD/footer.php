569cd6fc1f0ed27137f511bec37e993667a19533
ngantrinasgor <118157472+ngantrinasgor@users.noreply.github.com>
2023-02-09 09:12:33 +0700

    Add files via upload

---
+++ WEB-CRUD/footer.php
@@ -0,0 +1,4 @@
+<div class="fixed-bottom">
+        <footer class="p-3 bg-dark text-light d-flex justify-content-center"><p>Copyright &copy; <b><a href="https://www.instagram.com/jefffvvv/">@
+jefffvvv</a></b> All right reserved.</p></footer>
+    </div>
